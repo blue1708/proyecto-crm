088d909dbd7a7d98529cdfc59bc397dae85f799e
Adrian <aitoivan04@gmail.com>
2024-06-18 15:51:33 -0600

    probando error topbar

--- OneDrive/Escritorio/proyecto-cmd/src/Components/Topbar/Topbar.js
+++ OneDrive/Escritorio/proyecto-cmd/src/Components/Topbar/Topbar.js
@@ -1,44 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './Topbar.css';
-import { LogoutButton } from '../Logout/Logout'
-import Pidebar from '../Sidebar/Pidebar';
+import { LogoutButton } from '../Logout/Logout';
+import SidebarLat from '../Sidebar/Pidebar'
 
 const Topbar = () => {
-  const {user} = useAuth0();
-  const [showDropdown,setShowDropdown] = useState(false);
-  const toggleDropdown = ()=> {
-    setShowDropdown(!showDropdown);
-  };
+  const { user } = useAuth0();
 
   return (
-    <BrowserRouter>
-    <div className='topbar'>
-      <Routes>
-        <Route path='/' element={<Pidebar/>}/>
-      </Routes>
-      <Pidebar/>
-      <img src={user.picture} alt={user.name} onClick={toggleDropdown} />
-      {showDropdown && (
-          <div className="dropdown-container">
-            <div className="dropdown-content">
-              <img src={user.picture} alt={user.name} onClick={toggleDropdown} className='imagen_usuario'/>
-              <p>{user.email}</p>
-              <hr className="mb-3 mx-3 border-top-1 border-none surface-border" />
-              <div className="icons-container">
-                <i className="pi pi-comment" /> 
-                <i className="pi pi-envelope" /> 
-                <i className="pi pi-calendar" />
-              </div>
-              <LogoutButton/>
-              {/* Agrega más información aquí */}
-            </div>
-          </div>
-        )}
-      </div>
-      </BrowserRouter>
-  )
+    <div className="topbar">
+      <SidebarLat/>
+      <LogoutButton/>
+      <span className='saludo'>Welcome, {user.name}</span>
+      <img src={user.picture} alt={user.name} />
+    </div>
+  );
 };
 
 export default Topbar;
