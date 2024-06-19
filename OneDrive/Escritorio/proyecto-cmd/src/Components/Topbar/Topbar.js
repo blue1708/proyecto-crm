3d21fa2232d496f5cc8825671f92e5ae78b26b3d
Adrian <aitoivan04@gmail.com>
2024-06-19 15:44:13 -0600

    topbarjs

--- OneDrive/Escritorio/proyecto-cmd/src/Components/Topbar/Topbar.js
+++ OneDrive/Escritorio/proyecto-cmd/src/Components/Topbar/Topbar.js
@@ -1,41 +1,43 @@
 import React, { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { useNavigate } from 'react-router-dom';
-import './Topbar.css';
+import SidebarLat from '../Sidebar/SidebarLat'
 import { LogoutButton } from '../Logout/Logout';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './Topbar.css';
 
 const Topbar = () => {
   const { user } = useAuth0();
   const [showDropdown, setShowDropdown] = useState(false);
-  const navigate = useNavigate();
 
   const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
   };
 
-  const handleLogout = () => {
-    navigate(-1); // Regresa a la página anterior
-  };
-
   return (
-    <div className="topbar">
-      <img src={user.picture} alt={user.name} onClick={toggleDropdown} />
-      {showDropdown && (
-        <div className="dropdown-container">
-          <div className="dropdown-content">
-            <img src={user.picture} alt={user.name} onClick={toggleDropdown} className='imagen_usuario'/>
-            <p>{user.email}</p>
-            <hr className="mb-3 mx-3 border-top-1 border-none surface-border" />
-            <div className="icons-container">
-              <i className="pi pi-comment" /> 
-              <i className="pi pi-envelope" /> 
-              <i className="pi pi-calendar" />
+    <BrowserRouter>
+      <div className="topbar">
+        <Routes>
+          <Route path="/" element={<SidebarLat />} />
+        </Routes>
+        <img src={user.picture} alt={user.name} onClick={toggleDropdown} />
+        {showDropdown && (
+          <div className="dropdown-container">
+            <div className="dropdown-content">
+              <img src={user.picture} alt={user.name} onClick={toggleDropdown} className='imagen_usuario'/>
+              <p>{user.email}</p>
+              <hr className="mb-3 mx-3 border-top-1 border-none surface-border" />
+              <div className="icons-container">
+                <i className="pi pi-comment" /> 
+                <i className="pi pi-envelope" /> 
+                <i className="pi pi-calendar" />
+              </div>
+              <LogoutButton/>
+              {/* Agrega más información aquí */}
             </div>
-            <LogoutButton onClick={handleLogout} />
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </BrowserRouter>
   );
 };
 
