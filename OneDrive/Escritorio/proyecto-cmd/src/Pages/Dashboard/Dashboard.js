a6626172a517ad605a6f1ad8cef108a24c91a181
Adrian <aitoivan04@gmail.com>
2024-06-26 18:19:23 -0600

    dashboard actualizado y clientes

--- OneDrive/Escritorio/proyecto-cmd/src/Pages/Dashboard/Dashboard.js
+++ OneDrive/Escritorio/proyecto-cmd/src/Pages/Dashboard/Dashboard.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Card } from 'primereact/card';
+import './Dashboard.css';
 
 export default function Dashboard() {
+  useEffect(() => {}, []);
+
+  console.log('Rendering Dashboard');
   return (
-    <div>
-    <h2>Soy Un Dashboard y medire KPI's</h2>
+    <div className='graficos'>
+      <h2 className='titulo'>Estadisticas de Clientes</h2>
+      <hr className="mb-3 mx-3 border-top-2 border-bottom-1" />
+      <div className='tarjetas'>
+        <Card title="Cliente Nuevos" className='titulo_card'>
+          <hr className="mb-3 mx-3 border-top-2 border-bottom-1" />
+          <p className="m-0 dato">
+            1200
+          </p>
+        </Card>
+        <Card title="Clientes actuales" className='titulo_card'>
+          <hr className="mb-3 mx-3 border-top-2 border-bottom-1" />
+          <p className="m-0 dato">
+            5000
+          </p>
+        </Card>
+        <Card title="Clientes Totales" className='titulo_card'>
+          <hr className="mb-3 mx-3 border-top-2 border-bottom-1" />
+          <p className="m-0 dato">
+            6200
+          </p>
+        </Card>
+        <Card title="Clientes Perdidos" className='titulo_card'>
+          <hr className="mb-3 mx-3 border-top-2 border-bottom-1" />
+          <p className="m-0 dato">
+            100
+          </p>
+        </Card>
+      </div>
     </div>
-  )
+  );
 }
